e887ae9b4967b915cc734d95ce8a19e5e94c458b
keyzen <alonso.anavalon@gmail.com>
2021-03-08 16:52:01 -0300

    página terminada

---
+++ correo.php
@@ -0,0 +1,24 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $nombre = $_POST['name'];
+    $asunto = $_POST['asunto'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+    
+    $destinatario = "tonerkbt@gmail.com";
+    
+    $carta = "De: $nombre \n";
+    $carta .= "Correo: $email \n";
+    $carta .= "Asunto: $asunto \n";
+    $carta .= "Mensaje: $mensaje";
+    
+    mail ($destinatario, $asunto, $carta);
+    echo"<script>alert('Correo enviado exitosamente')</script>";
+    echo"<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+
+}
+
+
+?>
